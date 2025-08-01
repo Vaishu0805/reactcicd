2ac125e114ea01813dcdb437814c0b27997d4059
Vaishu0805 <chilakanagavaishnavi@gmail.com>
2025-08-01 23:12:04 +0530

    Fix: Remove invalid comment after </html> in index.html

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// Replace `your-username` and `your-repo-name` properly
 export default defineConfig({
   plugins: [react()],
+  base: '/reactcicd/',  // 👈 VERY IMPORTANT
 })
