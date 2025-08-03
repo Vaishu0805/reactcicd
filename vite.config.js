f48aa54aa4e049ecd7ccc303f1fbabcb6f1b6375
Vaishu0805 <chilakanagavaishnavi@gmail.com>
2025-08-03 15:52:47 +0530

    Fix: added vite.svg and updated config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
+  base: '/reactcicd/',  // 👈 VERY IMPORTANT
   plugins: [react()],
-});
+})
+
+
