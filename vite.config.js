bc7abc46287ca0922b24d6b578765e3a618b2d1e
Vaishu0805 <chilakanagavaishnavi@gmail.com>
2025-08-03 14:59:06 +0530

    Fix: Use plugin-react-swc in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/reactcicd/', // for GitHub Pages
-  plugins: [react()]
-})
+  plugins: [react()],
+});
